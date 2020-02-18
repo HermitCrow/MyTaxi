47eaca18b6cfab2d3b8896046eb8434f74568249
Emmanuel Efrayn Ulloa Domínguez <master1932@hotmail.com>
2020-02-18 15:24:57 -0400

    Create DataBase and First Table Taxi

---
+++ MyTaxi.Web/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyTaxi.Web.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyTaxi.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options)
+        {
+
+        }
+
+        public DbSet<TaxiEntity> Taxis { get; set; }
+    }
+}
---
+++ MyTaxi.Web/Data/Entities/TaxiEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyTaxi.Web.Data.Entities
+{
+    public class TaxiEntity
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The Field {0} is mandatory.")]
+        [StringLength(6, MinimumLength = 6, ErrorMessage = "The {0} field must have {1} characters.")]
+        [RegularExpression(@"^([A-Za-z]{3}\d{3})$",ErrorMessage = "The field {0} must have three characters and three numbers.")]
+        public string Plaque { get; set; }
+    }
+}
